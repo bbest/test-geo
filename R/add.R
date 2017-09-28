55609fd82331f4a697f8f23ad5ec0e6b3bc7a72d
Ben Best <ben@ecoquants.com>
2017-09-28 13:27:36 -0700

    init add()

---
+++ R/add.R
@@ -0,0 +1,13 @@
+#' add - staple it together
+#'
+#' @param x first element
+#' @param y the next one
+#' @param z yet another one
+#'
+#' @return additive result
+#' @export
+#'
+#' @examples
+#' add(2,2)
+add <- function(x, y, z=0){
+  x + y + z}
